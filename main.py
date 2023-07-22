7dc8a84ca136d777917f2bc52a58d1904914e2b6
yassin@siouda.com <yassin@siouda.com>
2023-07-23 00:11:52 +0200

    stream ;D

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlink
 from fastapi import FastAPI
-from fastapi.responses import FileResponse
+from fastapi.responses import FileResponse, StreamingResponse
 # cors
 from fastapi.middleware.cors import CORSMiddleware
 import subprocess
@@ -15,6 +15,70 @@ model = YOLO('yolov8x.pt')
 origins = ["*"]
 
 app = FastAPI()
+
+import time
+import asyncio
+
+
+async def streamVideo(url, fps=24):
+    cap = cv2.VideoCapture(url)
+    cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
+
+    frame_time = 1.0 / fps
+
+    while True:
+        try:
+            start_time = time.time()
+            ret, frame = cap.read()
+
+            if ret:
+                # Detect objects (if you want to)
+                results = model.predict(frame, conf=0.70, device=0,  verbose=False)
+                
+                #Display the box and label overlays
+                for result in results :
+                    boxes = result.boxes.xyxy
+                    i = 0
+                    for box in boxes:
+                        cls = int(result.boxes.cls[i])
+                        #print(cls)
+                        className = result.names[cls]
+
+                        xmin, ymin, xmax, ymax = int(box[0]), int(box[1]), int(box[2]), int(box[3])
+                        # Draw the bounding box
+                        cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (0, 0, 255), 2)
+                        # Draw label and confidence
+                        cv2.putText(frame, className, (xmin, ymin - 13), cv2.FONT_HERSHEY_SIMPLEX, 1e-3 * frame.shape[0], (0, 0, 255), 2)
+                        i = i + 1
+                    #await asyncio.sleep(3)
+
+                # limit capture buffer size
+                # Convert frame to bytes
+                _, buffer = cv2.imencode('.jpg', frame)
+
+                frame_bytes = buffer.tobytes()
+
+                yield (b'--frame\r\n'
+                       b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
+
+                elapsed_time = time.time() - start_time
+
+                # Adjust frame time based on actual processing time
+                frame_time_adjusted = max(frame_time - elapsed_time, 0)
+                #print(frame_time_adjusted)
+                await asyncio.sleep(frame_time_adjusted)
+
+        except KeyboardInterrupt:
+            break
+# realtime
+@app.get("/realtime/{user}")
+async def realtime(user: str):
+    # Open the stream
+    streams = streamlink.streams("https://www.twitch.tv/" + user)
+    url = streams["720p"].url   
+    #print(streams) 480p
+    
+    return StreamingResponse(streamVideo(url), media_type="multipart/x-mixed-replace;boundary=frame")
 
 # stream/user
 @app.get("/stream/{user}")
